3b3c3bd7cfce2125cfb645969d6a33e59c263cb6
hexd <hxdmaoming9527@163.com>
2026-08-07 11:37:42 +0800

    chore: clean up dome.py duplicated load_dotenv

--- examples/dome.py
+++ examples/dome.py
@@ -1,16 +1,15 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from dotenv import load_dotenv
-load_dotenv()
-
 from mini_smolagents import Agent, OpenAIModel, tool, web_search
+
 
 @tool
 def get_current_time() -> str:
     """获取当前时间"""
     from datetime import datetime
-    return datetime.now().strftime("%H:%M:%S")
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 model = OpenAIModel()
 agent = Agent(model=model, tools=[web_search, get_current_time])
